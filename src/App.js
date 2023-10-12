f8c1b57a9d2447eb2651573678761cbceee2b6a7
Muhammad Hashim <muhammadhashimsardar@gmail.com>
2023-10-12 19:25:46 +0500

    feat: :bento: Project Setuo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setuser] = useState("");
+  const [validatName, setvalidatName] = useState("")
+  const [userFoucs, setuserFoucs] = useState("")
+
+  const [password, setPassword] = useState("");
+    const [validatPassword, setvalidatPassword] = useState("");
+  const [matchPassword, setMtchPassword] = useState("");
+
+  return <div className="App">ggghjg</div>;
 }
 
 export default App;
